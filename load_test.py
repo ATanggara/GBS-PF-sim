aa57dc3c95a865665c4572d881b158482eb92ff5
andrew <andrew.tanggara@anu.edu.au>
2020-10-17 20:24:20 +1100

    reduce array script

---
+++ load_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 19:55:38 2020
+
+@author: andrewtanggara
+"""
+
+
+import numpy as np
+
+#m = 6
+#k = 4
+#reldir = "res_PForacle/"
+#resn = ["pfavgss", "errss"]
+
+x = np.load("res_PForacle/errss(6,4)_0.4_0.4_0.5_-1.0_1.0 - 2020-10-17-17-111_10th.npy")
+
+y = np.load("res_PForacle/errss(6,4)_0.4_0.4_0.5_-1.0_1.0 - 2020-10-17-17-111.npy")
---
+++ trim_npy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 16:59:11 2020
+
+to run this:
+    $ python trim_npy.py sim_details n
+
+where
+    - sim_details: file name after the data description without ".npy"
+    - n: only take every n-th element in array
+
+@author: andrewtanggara
+"""
+
+import numpy as np
+import sys
+import os
+
+m = sys.argv[1]
+k = sys.argv[2]
+sim_details = sys.argv[3]
+timestamp = sys.argv[4]
+n = int(sys.argv[5])
+
+reldir = "res_PForacle/"
+resn = ["pfavgss", "errss"]
+lress = []
+
+for i in range(len(resn)):
+    lress.append(np.load(
+            reldir+resn[i]+"("+m+","+k+")"+
+            "_"+sim_details+" - "+timestamp+".npy"))
+
+for i in range(len(lress)):
+    lress[i] = np.array(lress[i])[:,::n]
+    np.save(reldir+str(n)+"th"+"_"+resn[i]+"("+m+","+k+")"+"_"+sim_details+
+            " - "+timestamp, lress[i])
+
